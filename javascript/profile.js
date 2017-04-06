ff88d377bdce60a1518f3db11bcd74875752c52f
Dongmin <deexpabada@gmail.com>
2017-04-05 19:21:03 -0500

    Basic html, css, js

---
+++ javascript/profile.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Dopamine on 4/5/2017.
+ */
+
+function user(firstName, lastName, year, profilePic, bio, major, gender) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.year = year;
+    this.profilePic = profilePic;
+    this.bio = bio;
+    this.major = major;
+    this.gender = gender;
+}
